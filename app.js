a5a2a0f4a9aa3ec5ae648bd66e43b57e2f6b326c
git-mhasan <mahadi2020dev@gmail.com>
2022-02-28 05:26:12 +0600

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("hello.");
+const getElemtntByID = id => document.getElementById(id);
+
+// const mainPanel = getElemtntByID('main-panel');
+// const sidePanel = getElemtntByID('side-panel');
+
+
+// mainPanel.style.display = 'block'
+// sidePanel.style.display = 'block'
+// mainPanel.classList.add = 'bg-success'
+// sidePanel.classList.add = 'col-6'
+
+
+
+const inputText = getElemtntByID('input-txt');
+
+document.getElementById('search-btn').addEventListener('click', function () {
+    if (!inputText.value) {
+        // console.log('boo');
+    } else if (inputText.value.trim().length === 0) {
+        // console.log('all space');
+        inputText.value = '';
+    } else {
+        const mealUrl = `www.themealdb.com/api/json/v1/1/search.php?s=${inputText.value.trim()}`;
+
+        console.log(mealUrl);
+        inputText.value = '';
+
+    }
+
+})
