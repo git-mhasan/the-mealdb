4abc11db98ff43013d60a0a85f5ff99bc08b8d2c
git-mhasan <mahadi2020dev@gmail.com>
2022-02-28 09:59:13 +0600

    api call and showing image in card

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 const getElemtntByID = id => document.getElementById(id);
 
-// const mainPanel = getElemtntByID('main-panel');
-// const sidePanel = getElemtntByID('side-panel');
-
-
-// mainPanel.style.display = 'block'
-// sidePanel.style.display = 'block'
-// mainPanel.classList.add = 'bg-success'
-// sidePanel.classList.add = 'col-6'
-
-
 
 const inputText = getElemtntByID('input-txt');
 
@@ -20,11 +10,42 @@ document.getElementById('search-btn').addEventListener('click', function () {
         // console.log('all space');
         inputText.value = '';
     } else {
-        const mealUrl = `www.themealdb.com/api/json/v1/1/search.php?s=${inputText.value.trim()}`;
+        const mealUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText.value.trim()}`;
 
-        console.log(mealUrl);
+        searchMeal(mealUrl);
         inputText.value = '';
 
     }
 
 })
+
+const searchMeal = async urlString => {
+    const mainDiv = getElemtntByID('main-panel');
+
+    const response = await fetch(urlString);
+    const data = await response.json();
+    data.meals?.forEach(element => {
+        console.log(element);
+        const mealCard = document.createElement('div');
+        mealCard.classList.add('card', 'mb-3');
+        mealCard.style.maxWidth = '540px';
+        mealCard.innerHTML = `
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${element.strMealThumb}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>  
+        `
+        mainDiv.appendChild(mealCard);
+    });
+    // console.log(data.meals);
+
+}
